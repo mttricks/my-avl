223084ddb8ac75ca04bb5db16417f50bcc246d72
mttricks <70296532+mttricks@users.noreply.github.com>
2020-12-31 10:49:55 -0700

    Update NodeInterface.h

--- NodeInterface.h
+++ NodeInterface.h
@@ -1,6 +1,4 @@
-//YOU MAY NOT MODIFY THIS DOCUMENT
-/*
-*/
+
 #pragma once
 using namespace std;
 
@@ -33,11 +31,7 @@ public:
 
 	/*
 	* Returns the height of this node. The height is the number of nodes
-	* along the longest path from this node to a leaf.  While a conventional
-	* interface only gives information on the functionality of a class and does
-	* not comment on how a class should be implemented, this function has been
-	* provided to point you in the right direction for your solution.  For an
-	* example on height, see page 448 of the text book.
+	* along the longest path from this node to a leaf. 
 	*
 	* @return the height of this tree with this node as the local root.
 	*/
